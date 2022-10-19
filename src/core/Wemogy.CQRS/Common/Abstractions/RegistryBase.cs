c3fa1c8c1b3e6771268c159d14197f75cbc85feb
Sebastian Küsters <sebastian@wemogy.com>
2022-10-19 10:11:20 +0200

    Fixed concurrency issue in RegistryBase

--- src/core/Wemogy.CQRS/Common/Abstractions/RegistryBase.cs
+++ src/core/Wemogy.CQRS/Common/Abstractions/RegistryBase.cs
@@ -1,20 +1,14 @@
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace Wemogy.CQRS.Common.Abstractions;
 
 public abstract class RegistryBase<TKey, TValue>
 {
-    private static readonly Dictionary<TKey, TValue> EntriesCache = new Dictionary<TKey, TValue>();
+    private static readonly ConcurrentDictionary<TKey, TValue> EntriesCache = new ConcurrentDictionary<TKey, TValue>();
 
     protected TValue GetRegistryEntry(TKey key)
     {
-        if (!EntriesCache.TryGetValue(key, out TValue value))
-        {
-            value = InitializeEntry(key);
-            EntriesCache.Add(key, value);
-        }
-
-        return value;
+        return EntriesCache.GetOrAdd(key, InitializeEntry);
     }
 
     protected abstract TValue InitializeEntry(TKey key);
