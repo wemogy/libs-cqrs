37a7d09e7c895e2349da3126048328f93fce93e9
Sebastian Küsters <sebastian@wemogy.com>
2023-12-05 08:34:15 +0100

    Added Activities to Queries

--- src/core/Wemogy.CQRS/Queries/Runners/QueryRunner.cs
+++ src/core/Wemogy.CQRS/Queries/Runners/QueryRunner.cs
@@ -24,17 +24,23 @@ public class QueryRunner<TQuery, TResult>
 
     public async Task<TResult> QueryAsync(TQuery query, CancellationToken cancellationToken)
     {
+        using var activity = Observability.DefaultActivities.StartActivity($"{typeof(TQuery).Name} Query");
         foreach (var validator in _queryValidators)
         {
+            using var validatorActivity = Observability.DefaultActivities.StartActivity($"{typeof(TQuery).Name} QueryValidator");
             validator.Validate(query);
         }
 
         foreach (var authorization in _queryAuthorizations)
         {
+            using var authorizationActivity = Observability.DefaultActivities.StartActivity($"{typeof(TQuery).Name} QueryAuthorization");
             await authorization.AuthorizeAsync(query);
         }
 
+        using var queryHandlerActivity = Observability.DefaultActivities.StartActivity($"{typeof(TQuery).Name} QueryHandler");
         var result = await _queryHandler.HandleAsync(query, cancellationToken);
+        queryHandlerActivity?.Stop();
+
         return result;
     }
 }
